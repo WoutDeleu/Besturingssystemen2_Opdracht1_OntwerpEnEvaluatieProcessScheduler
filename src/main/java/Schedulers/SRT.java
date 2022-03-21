138df44f9d99664b0c5f2e41c265d0dae3c6014f
WoutDeleu <80529253+WoutDeleu@users.noreply.github.com>
2022-03-21 19:00:59 +0100

    (Poging tot) SRT Werkt net niet....

--- src/main/java/Schedulers/SRT.java
+++ src/main/java/Schedulers/SRT.java
@@ -19,38 +19,42 @@ public class SRT {
         Process temp = new Process(adjustableList.get(0));
         adjustableList.remove(0);
 
+
+        //FOUT: wanneer adjustableList isEmpty => cannot : adjustableList.get(0) (lijn 25) lukt niet....
         while(!adjustableList.isEmpty() || !waiting.isEmpty()) {
-            if( (adjustableList.get(0).getArrivaltime() - timer) <= temp.getServicetime() ) {
-                //A new process is being added to the queue
-                waiting.add(adjustableList.get(0));
-                temp.setServicetime(temp.getServicetime() - (adjustableList.get(0).getArrivaltime() - timer));
-                timer = adjustableList.get(0).getArrivaltime();
+                if ((adjustableList.get(0).getArrivaltime() - timer) <= temp.getServicetime()) {
+                    //A new process is being added to the queue
+                    waiting.add(adjustableList.get(0));
+                    temp.setServicetime(temp.getServicetime() - (adjustableList.get(0).getArrivaltime() - timer));
+                    timer = adjustableList.get(0).getArrivaltime();
 
-                if(adjustableList.get(0).getServicetime() < temp.getServicetime()) {
-                    //New process has shorter remaining time as other process
-                    waiting.add(new Process(temp));
-                    temp = adjustableList.get(0);
-                    temp.setStarttime(timer);
-                }
+                    if (adjustableList.get(0).getServicetime() < temp.getServicetime()) {
+                        //New process has shorter remaining time as other process
+                        waiting.add(new Process(temp));
+                        temp = adjustableList.get(0);
+                        temp.setStarttime(timer);
+                    }
 
-                adjustableList.remove(0);
-            }
-            else {
-                //current process is being finished
-                timer += temp.getServicetime();
-                processes.get(temp.getId()-1).setEndtime(timer);
-                processes.get(temp.getId()-1).setStarttime(temp.getStarttime());
-                if(!waiting.isEmpty()) {
-                    temp = findSRT(waiting);
-                    temp.setStarttime(timer);
-                }
-                else {
-                    timer += adjustableList.get(0).getServicetime();
                     adjustableList.remove(0);
-                    temp = adjustableList.get(0);
-                    temp.setStarttime(timer);
                 }
-            }
+
+                else {
+                    //current process is being finished
+                    timer += temp.getServicetime();
+                    processes.get(temp.getId() - 1).setEndtime(timer);
+                    processes.get(temp.getId() - 1).setStarttime(temp.getStarttime());
+                    if (!waiting.isEmpty()) {
+                        temp = findSRT(waiting);
+                        temp.setStarttime(timer);
+                    }
+                    else {
+                        timer += adjustableList.get(0).getServicetime();
+                        adjustableList.remove(0);
+                        temp = adjustableList.get(0);
+                        temp.setStarttime(timer);
+                    }
+                }
+
         }
         calculateValues(processes);
         return processes;
