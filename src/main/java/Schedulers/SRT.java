501a1bea3897965a8e9045adc0354978ae6e6383
WoutDeleu <80529253+WoutDeleu@users.noreply.github.com>
2022-03-24 10:02:01 +0100

    (Poging tot) SRT Werkt net niet....

--- src/main/java/Schedulers/SRT.java
+++ src/main/java/Schedulers/SRT.java
@@ -16,58 +16,92 @@ public class SRT {
         //int currentIndexTimer = 0;
         long timer = adjustableList.get(0).getArrivaltime();
 
-        Process temp = new Process(adjustableList.get(0));
+        Process current = new Process(adjustableList.get(0));
         adjustableList.remove(0);
 
 
-        //FOUT: wanneer adjustableList isEmpty => cannot : adjustableList.get(0) (lijn 25) lukt niet....
+        //FOUT::: Bij bepaalde vult hij de endtijd niet in?? Starttime wordt wel ingevuld!!!!
         while(!adjustableList.isEmpty() || !waiting.isEmpty()) {
-                if ((adjustableList.get(0).getArrivaltime() - timer) <= temp.getServicetime()) {
+            if(waiting.isEmpty()) {
+                if (isNextProcessFirst(adjustableList, current, timer)) {
                     //A new process is being added to the queue
-                    waiting.add(adjustableList.get(0));
-                    temp.setServicetime(temp.getServicetime() - (adjustableList.get(0).getArrivaltime() - timer));
+                    current.setServicetime(current.getServicetime() - (adjustableList.get(0).getArrivaltime() - timer));
                     timer = adjustableList.get(0).getArrivaltime();
 
-                    if (adjustableList.get(0).getServicetime() < temp.getServicetime()) {
+                    if (adjustableList.get(0).getServicetime() < current.getServicetime()) {
                         //New process has shorter remaining time as other process
-                        //Matthieu: volgens mij moet ge geen nieuw process toevoegen ma gwn temp omdaje zo de referentie naar het object doorgeeft
-                        // als je een "new Process() doorgeeft wordt alles wa er daarna aan wordt gewijzigd niet doorgevoerd op het object in de lijst processes3
-                        ///////////////////////////////
-                        waiting.add(new Process(temp));
-                        ///////////////////////////////
-                        temp = adjustableList.get(0);
-                        temp.setStarttime(timer);
-                    }
-
-                    adjustableList.remove(0);
-                }
-
-                else {
-                    //current process is being finished
-                    timer += temp.getServicetime();
-                    processes.get(temp.getId() - 1).setEndtime(timer);
-                    processes.get(temp.getId() - 1).setStarttime(temp.getStarttime());
-                    if (!waiting.isEmpty()) {
-                        temp = findSRT(waiting);
-                        temp.setStarttime(timer);
+                        replaceCurrent(current, waiting, adjustableList, timer);
                     }
                     else {
-                        timer += adjustableList.get(0).getServicetime();
-                        adjustableList.remove(0);
-                        temp = adjustableList.get(0);
-                        temp.setStarttime(timer);
+                        waiting.add(adjustableList.get(0));
                     }
+                    adjustableList.remove(0);
                 }
+                else {
+                    //current process is being finished
+                    timer += current.getServicetime();
+                    finishCurrent(processes, current, timer);
+                    timer = adjustableList.get(0).getArrivaltime();
+                    current = adjustableList.get(0);
+                    current.setStarttime(timer);
+                    adjustableList.remove(0);
+                }
+            }
+            else if(adjustableList.isEmpty()) {
+                timer += current.getServicetime();
+                finishCurrent(processes, current, timer);
+                current = findSRT(waiting);
+                current.setStarttime(timer);
 
+            }
+            else {
+                if (isNextProcessFirst(adjustableList, current, timer)) {
+                    //A new process is being added to the queue
+                    current.setServicetime(current.getServicetime() - (adjustableList.get(0).getArrivaltime() - timer));
+                    timer = adjustableList.get(0).getArrivaltime();
+
+                    if (adjustableList.get(0).getServicetime() < current.getServicetime()) {
+                        //New process has shorter remaining time as other process
+                        replaceCurrent(current, waiting, adjustableList, timer);
+                    }
+                    else {
+                        waiting.add(adjustableList.get(0));
+                    }
+                    adjustableList.remove(0);
+                }
+                else {
+                    //current process is being finished
+                    timer += current.getServicetime();
+                    finishCurrent(processes, current, timer);
+                    current = findSRT(waiting);
+                    current.setStarttime(timer);
+                }
+            }
         }
+        //finishCurrent(processes, current, timer);
+        findEndTimeNull(processes);
         calculateValues(processes);
         return processes;
+    }
+
+    private boolean isNextProcessFirst(List<Process> adjustableList, Process current, long timer) {
+        return (adjustableList.get(0).getArrivaltime() - timer) <= current.getServicetime();
+    }
+    private void replaceCurrent(Process current, Queue<Process> waiting, List<Process> adjustableList, long timer) {
+        waiting.add(new Process(current));
+        current = adjustableList.get(0);
+        current.setStarttime(timer);
+    }
+
+    private void finishCurrent(List<Process> processes, Process current, long timer) {
+        processes.get(current.getId() - 1).setEndtime(timer);
+        processes.get(current.getId() - 1).setStarttime(current.getStarttime());
     }
 
     private Process findSRT(Queue<Process> waiting) {
         Process shortest = waiting.peek();
         for(Process p : waiting) {
-           if(p.getServicetime() <= shortest.getServicetime()) shortest = p;
+            if(p.getServicetime() <= shortest.getServicetime()) shortest = p;
         }
         waiting.remove(shortest);
         return shortest;
@@ -78,6 +112,12 @@ public class SRT {
             p.setWaittime(p.getEndtime() - p.getServicetime() - p.getArrivaltime());
             p.setTat(p.getWaittime() + p.getServicetime());
             p.setGenTat(p.getTat()/p.getServicetime());
+        }
+    }
+
+    private void findEndTimeNull(List<Process> processes) {
+        for(Process p : processes) {
+            if(p.getEndtime() == 0) System.out.println(p.getId());
         }
     }
 }
